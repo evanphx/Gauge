bc580d8f80b5afbc7bdc7a3fa2d401f131fa1a48
Brian Shirai <brixen@gmail.com>
2014-01-28 16:32:30 -0600

    Fixed overwriting DUMP env var.

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,15 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Set ENV['DUMP'] as such:
-ENV['DUMP'] = File.expand_path("heap.dump")
+
+# Set the DUMP environment variable to the heap dump file. For example:
+#
+#   export DUMP=/path/to/heap.dump
+#
+ENV['DUMP'] = File.expand_path("heap.dump") unless ENV['DUMP']
 
 # Quick error class for config issue and check for dump file loading
 class ConfigurationError < StandardError; end
-
-if !ENV['DUMP']
-  raise ConfigurationError, "Set ENV variable for dump file."
-end
 
 # Initialize the rails application
 Gauge::Application.initialize!
